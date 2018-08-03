f2d71ede303133b8a9c570622f1b24e7729a4639
Brian Shef <brian.shef@rewardstyle.com>
2018-08-03 10:07:05 -0500

    Initialize and draw screen with tdl

--- arroguella/game.py
+++ arroguella/game.py
@@ -1,4 +1,27 @@
-#!/bin/python
+import tdl
+
+# Constants
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+FONT='arroguella/fonts/consolas_unicode_12x12.png'
+LIMIT_FPS = 20
+
+
+def initialize():
+    tdl.set_font(FONT, greyscale=False, altLayout=False)
+    console = tdl.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="Arroguella", fullscreen=False)
+    tdl.setFPS(LIMIT_FPS)
+    return console
+
+
+def run_main_loop(console):
+    while not tdl.event.is_window_closed():
+        console.draw_char(1, 1, '@', bg=None, fg=(255,255,255))
+        tdl.flush()
+
 
 def run():
-    print ('Hello, Arroguella!')
+    print ('Starting Arroguella ... ')
+    console = initialize()
+    run_main_loop(console)
+    print (' ... exited Arroguella.')
